ba499fb6a4e3dc1ce740ce9672bdbd9b165dd311
myrmidon <dada3mn@gmail.com>
2015-04-19 20:41:08 +0200

    Initial Commit

---
+++ Stop Watching/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stop Watching
+//
+//  Created by Abdullah on 19/04/15.
+//  Copyright (c) 2015 motjuste. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
