a07fc1eeb14c6b4065eefcff4cb0e32c5410767e
myrmidon <dada3mn@gmail.com>
2015-04-21 21:58:52 +0200

    WatchValues is now a different struct For data persistence later I guess

--- Stop Watching/Watch.swift
+++ Stop Watching/Watch.swift
@@ -8,38 +8,45 @@
 
 import Foundation
 
+struct WatchValues {
+    var fractions: Int
+    var seconds: Int
+    var minutes: Int
+}
+
 class Watch {
-    var fractions: Int = 0
-    var seconds: Int = 0
-    var minutes: Int = 0
+    var values: WatchValues
     
     let incrementMod100 = makeModuloIncrementor(100)
     let incrementMod60 = makeModuloIncrementor(60)
     
-    
     var watchString: String {
         get {
-            let fractionsString = fractions > 9 ? "\(fractions)" : "0\(fractions)"
-            let secondsString = seconds > 9 ? "\(seconds)" : "0\(seconds)"
-            let minutesString = minutes > 9 ? "\(minutes)" : "0\(minutes)"
+            let fractionsString = values.fractions > 9 ? "\(values.fractions)" : "0\(values.fractions)"
+            let secondsString = values.seconds > 9 ? "\(values.seconds)" : "0\(values.seconds)"
+            let minutesString = values.minutes > 9 ? "\(values.minutes)" : "0\(values.minutes)"
             
             return "\(minutesString):\(secondsString),\(fractionsString)"
         }
     }
     
+    init (fractions: Int = 0, seconds: Int = 0, minutes: Int = 0) {
+        values = WatchValues(fractions: fractions, seconds: seconds, minutes: minutes)
+    }
+    
     func incrementWatch() -> (Bool) {
         var overshot: Bool = false
         
-        if incrementMod100(value: &fractions) {
-            if incrementMod60(value: &seconds) {
-                overshot = incrementMod100(value: &minutes)
+        if incrementMod100(value: &values.fractions) {
+            if incrementMod60(value: &values.seconds) {
+                overshot = incrementMod100(value: &values.minutes)
             }
         }
         return overshot
     }
     
     func resetWatch() {
-        (fractions, seconds, minutes) = (0, 0, 0)
+        values = WatchValues(fractions: 0, seconds: 0, minutes: 0)
     }
 }
 
