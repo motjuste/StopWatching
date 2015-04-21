1d26126443124901a0b2650e44a12ebe7505a129
myrmidon <dada3mn@gmail.com>
2015-04-21 22:00:23 +0200

    Make UITableViewCell really reusable; added comments

--- Stop Watching/ViewController.swift
+++ Stop Watching/ViewController.swift
@@ -15,30 +15,36 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     var currentWatch = Watch()
     var laps = [String]()
     
-    var stopwatchRunning: Bool = false
-    var resetEnabled: Bool = false
-    
     @IBOutlet weak var totalUILabel: UILabel!
     @IBOutlet weak var currentUILabel: UILabel!
     @IBOutlet weak var stopresetButton: UIButton!
     @IBOutlet weak var lapsTableView: UITableView!
     
+    var stopwatchRunning: Bool = false
+    var resetEnabled: Bool = false  // Reset enabled only when stopwatch is stopped
+    
     @IBAction func screenTap(sender: AnyObject) {
         if stopwatchRunning {
-            currentWatch.resetWatch()
-            currentUILabel.text = currentWatch.watchString
+
+            // Reset currentWatch
+            currentWatch.resetWatch(); currentUILabel.text = currentWatch.watchString
             
-            laps.insert(totalWatch.watchString, atIndex: 0)
-            lapsTableView.reloadData()
+            // Add Lap
+            laps.insert(totalWatch.watchString, atIndex: 0); lapsTableView.reloadData()
+            
+            
         } else {
-            timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: "updateLabels", userInfo: nil, repeats: true)
             
+            // Start Timer
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: "updateLabels", userInfo: nil, repeats: true)
+            stopwatchRunning = true
+            
+            // Enable and Unhide stopresetButton as Stop
             stopresetButton.enabled = true
             stopresetButton.hidden = false
             stopresetButton.setImage(UIImage(named: "stop.png"), forState: .Normal)
             
-            stopwatchRunning = true
-            
+            // If stop watch started after Stop, scroll lapsTableView to top and make un-scrollable
             if resetEnabled {
                 resetEnabled = false
                 lapsTableView.scrollToRowAtIndexPath(NSIndexPath (forRow: 0, inSection: 0), atScrollPosition: UITableViewScrollPosition.Top, animated: true)
@@ -49,39 +55,47 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     @IBAction func stopreset(sender: AnyObject) {
         if resetEnabled {
-            totalWatch.resetWatch()
-            totalUILabel.text = totalWatch.watchString
-            currentWatch.resetWatch()
-            currentUILabel.text = currentWatch.watchString
-            
-            laps.removeAll(keepCapacity: false)
-            lapsTableView.reloadData()
-            
-            stopresetButton.enabled = false
-            stopresetButton.hidden = true
             resetEnabled = false
             
-            stopwatchRunning = false
+            // Reset the stopwatches
+            totalWatch.resetWatch(); totalUILabel.text = totalWatch.watchString
+            currentWatch.resetWatch(); currentUILabel.text = currentWatch.watchString
             
+            // Reset laps and lapsTableView
+            laps.removeAll(keepCapacity: false)
+            lapsTableView.reloadData()
             lapsTableView.scrollEnabled = false
+            
+            // Disable and hide stopresetButton
+            stopresetButton.enabled = false
+            stopresetButton.hidden = true
         } else {
+
+            // Stop timer
             timer.invalidate()
             stopwatchRunning = false
+            
+            // Change stopresetButton to Reset
             stopresetButton.setImage(UIImage(named: "reset.png"), forState: .Normal)
             resetEnabled = true
             
+            // make lapsTableView scrollable
             lapsTableView.scrollEnabled = true
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Initialize stopwatches
         totalUILabel.text = totalWatch.watchString
         currentUILabel.text = currentWatch.watchString
+        
+        // Disable and hide stopresetButton
         stopresetButton.enabled = false
         stopresetButton.hidden = true
         
+        // make lapsTableView un-scrollable
         lapsTableView.scrollEnabled = false
     }
 
@@ -91,33 +105,37 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func updateLabels () {
-        totalWatch.incrementWatch()
-        totalUILabel.text = totalWatch.watchString
-        currentWatch.incrementWatch()
-        currentUILabel.text = currentWatch.watchString
+        totalWatch.incrementWatch(); totalUILabel.text = totalWatch.watchString
+        currentWatch.incrementWatch(); currentUILabel.text = currentWatch.watchString
     }
 
     // Table View Methods
-    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var cell = UITableViewCell(style: UITableViewCellStyle.Value2, reuseIdentifier: "Cell")
+        var cell: UITableViewCell! = tableView.dequeueReusableCellWithIdentifier("Cell") as? UITableViewCell
         
-        cell.backgroundColor = self.view.backgroundColor
-        cell.textLabel?.text = "Lap \(laps.count - indexPath.row)"
-        cell.textLabel?.font = UIFont(name: "HelvticaNeue-Thin", size: 29)
-        cell.textLabel?.textColor = UIColor.blackColor()
-        cell.textLabel?.textAlignment = NSTextAlignment.Left
-        cell.detailTextLabel?.text = laps[indexPath.row]
-        cell.detailTextLabel?.font = UIFont(name: "HelveticaNeue-UltraLight", size: 29)
+        // if a reusable UITableViewCell "Cell" is not available, create one
+        if (cell == nil) {
+            cell = UITableViewCell(style: UITableViewCellStyle.Value2, reuseIdentifier: "Cell")  // UITableViewCellStyle.Value2 has two labels, first: Blue, aligned Right, second: Tint, aligned left
+            
+            // Customize cell
+            cell.backgroundColor = self.view.backgroundColor
+            
+            cell.textLabel!.font = UIFont(name: "HelveticaNeue-Thin", size: 24)
+            cell.textLabel!.textColor = UIColor.blackColor()
+            cell.textLabel!.textAlignment = NSTextAlignment.Left
+            cell.detailTextLabel!.font = UIFont(name: "HelveticaNeue-UltraLight", size: 29)
+        }
+        
+        // Add recent lap to top
+        cell.textLabel!.text = "Lap \(laps.count - indexPath.row)"
+        cell.detailTextLabel!.text = laps[indexPath.row]
         
         return cell
-        
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return laps.count
     }
-    
     
 }
 
